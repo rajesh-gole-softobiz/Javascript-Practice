9092b08f970b4d94cd03873e94e0faa053858ccd
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-18 20:07:56 +0530

    Onclick Event

---
+++ 2.Onclick-Event/script.js
@@ -0,0 +1,9 @@
+var myVar = document.querySelector("#img1");
+
+function myfun1() {
+    myVar.src = "https://cdn.pixabay.com/photo/2016/05/17/22/16/baby-1399332_1280.jpg";
+}
+
+function myfun2(){
+    myVar.src = "https://cdn.pixabay.com/photo/2018/05/09/01/00/greece-3384386_1280.jpg";
+}
