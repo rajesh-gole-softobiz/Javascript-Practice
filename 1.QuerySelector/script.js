aa520fd31a945033562661cb47687fb12f6229e7
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-18 19:42:27 +0530

    Query Selector

---
+++ 1.QuerySelector/script.js
@@ -0,0 +1,11 @@
+document.getElementById('myPara1').innerHTML = "Paragraph changed";
+
+document.querySelector('#myPara2').innerHTML = "Paragraph changed by Query Selector";
+
+document.querySelector('h1').innerHTML = "Header changed by Tag name";
+
+
+document.querySelector('.myList li a').innerHTML = 'new apple';
+
+
+document.querySelectorAll('h4')[1].innerHTML = 'Text Changed by Query Selector All';
