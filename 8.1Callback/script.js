bfe1e4d1091c884f91f8954baf21eb38519c25c7
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-20 15:30:23 +0530

    callback

--- 8.1Callback/script.js
+++ 8.1Callback/script.js
@@ -1,33 +1,42 @@
-// Asynchronous
-const taskOne = () =>{
-    console.log("Task1");
+// // Asynchronous
+// const taskOne = () =>{
+//     console.log("Task1");
+// }
+
+// // This is Asynchronous
+// const dataLoading = () => {
+//     console.log("Task2 dataLoading");
+// }
+
+// const taskTwo = () =>{
+// setTimeout(dataLoading,3000);
+// }
+// const taskThree = () =>{
+// console.log("Task3");
+// }
+// const taskFour = () =>{
+// console.log("Task4");
+// }
+// const taskFive = () =>{
+// console.log("Task5");
+// }
+
+
+// taskOne();
+// taskTwo();
+// taskThree();
+// taskFour();
+// taskFive();
+
+
+// Callback function
+function square(x){
+    console.log(`Square  of ${x} : ${x*x}`);
 }
 
-// This is Asynchronous
-const dataLoading = () => {
-    console.log("Task2 dataLoading");
+function higherOrderFunction(num, callback){
+    callback(num); // here callback is the square
 }
 
-const taskTwo = () =>{
-setTimeout(dataLoading,3000);
-}
-const taskThree = () =>{
-console.log("Task3");
-}
-const taskFour = () =>{
-console.log("Task4");
-}
-const taskFive = () =>{
-console.log("Task5");
-}
-
-
-taskOne();
-taskTwo();
-taskThree();
-taskFour();
-taskFive();
-
-
-
+higherOrderFunction(5,square);
 
