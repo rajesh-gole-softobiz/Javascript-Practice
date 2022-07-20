8027caf2d11e980b15ed5134999c101a64cf9db6
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-20 16:03:30 +0530

    callback and higher order function

--- 8.1Callback/script.js
+++ 8.1Callback/script.js
@@ -1,4 +1,4 @@
-// // Asynchronous
+// //Code-1: Asynchronous
 // const taskOne = () =>{
 //     console.log("Task1");
 // }
@@ -29,14 +29,41 @@
 // taskFive();
 
 
-// Callback function
-function square(x){
-    console.log(`Square  of ${x} : ${x*x}`);
+// //Code-2: Callback function
+// function square(x){
+//     console.log(`Square  of ${x} : ${x*x}`);
+// }
+
+// function higherOrderFunction(num, callback){
+//     callback(num); // here callback is the square
+// }
+
+// higherOrderFunction(5,square);
+
+
+
+//Code-3: Call back with asynchronous : first Task-1 will display, then Task-2, then Task-3
+const taskOne = (callback) =>{
+    console.log("Task1");
+    callback();
 }
 
-function higherOrderFunction(num, callback){
-    callback(num); // here callback is the square
+
+const taskTwo = (callback) =>{
+setTimeout(() => {
+    console.log("Task2 dataLoading");
+    callback();
+},3000);
 }
 
-higherOrderFunction(5,square);
+const taskThree = () =>{
+console.log("Task3");
+}
+
+taskOne(function f1() {
+    taskTwo( () => {
+        taskThree();
+    });
+});
+
 
