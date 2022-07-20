aa676745e14361cb509bd42ed7125872bd05c1d5
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-20 16:59:49 +0530

    promise chaining

---
+++ 8.3Promise-Chaining/script.js
@@ -0,0 +1,49 @@
+console.log("Welcome");
+
+const taskOne = () => {
+    return new Promise((resolve,reject) =>{
+        resolve('Task-1 is completed');
+    })
+}
+
+const taskTwo = () => {
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            resolve('Task-2 is completed');
+        },2000);
+    })
+}
+
+const taskThree = () => {
+    return new Promise((resolve,reject) =>{
+        resolve('Task-3 is completed');
+    })
+}
+
+const taskFour = () => {
+    return new Promise((resolve,reject) =>{
+        resolve('Task-4 is completed');
+    })
+}
+
+//1st -> Task1 completed, then wait for 2s to complete Task2,then Task3,Task4
+taskOne()
+.then((res)=> console.log(res))
+.then(taskTwo)
+.then((res)=> console.log(res))
+.then(taskThree)
+.then((res)=> console.log(res))
+.then(taskFour)
+.then((res)=> console.log(res))
+
+
+// taskOne()
+//     .then((res)=> console.log(res));
+// taskTwo()
+//     .then((res)=> console.log(res));
+// taskThree()
+//     .then((res)=> console.log(res));
+// taskFour()
+//     .then((res)=> console.log(res));
+
+console.log("end");
